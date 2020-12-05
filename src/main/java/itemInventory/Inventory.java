ecf2f89eefb36f125306e5ca330026a39f943cd6
Renata Kazragyte <777akata777@gmail.com>
2020-12-05 10:42:22 +0200

    class work 2020-11-22

--- src/main/java/itemInventory/Inventory.java
+++ src/main/java/itemInventory/Inventory.java
@@ -1,27 +1,27 @@
-package itemInventory;
-
-import edu.Person;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class Inventory implements Item{
-        private int quantity;
-        private float weight;
-
-    public Inventory(int quantity) {
-        this.quantity = quantity;
-    }
-
-    //    public ItemInventory(Person[]people) {
-//        for (Person person:people) {
-//            add(person);
-//        }
+//package itemInventory;
+//
+//
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class Inventory implements Item{
+//        private int quantity;
+//        private float weight;
+//
+//    public Inventory(int quantity) {
+//        this.quantity = quantity;
 //    }
-//    public void add (Item item){
-//        item.add(item.getName(), item.getPrice());
-//    }
-//    public Person get (Long ID){
-//        return people.get(ID);
-//    }
-}
+//
+//    //    public ItemInventory(Person[]people) {
+////        for (Person person:people) {
+////            add(person);
+////        }
+////    }
+////    public void add (Item item){
+////        item.add(item.getName(), item.getPrice());
+////    }
+////    public Person get (Long ID){
+////        return people.get(ID);
+////    }
+//}
